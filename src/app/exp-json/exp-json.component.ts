fd13172d4ae558a06605e4bf8b8df914c828494f
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-02-01 08:50:32 -0300

    fim do exp-json

--- src/app/exp-json/exp-json.component.ts
+++ src/app/exp-json/exp-json.component.ts
@@ -10,14 +10,49 @@ import { CarsService } from '../services/cars.service';
 export class ExpJsonComponent {
 
   title = 'todo';
-  carros: iCar[] = [];
+  // carros: iCar[] = [];
 
   constructor(private carService: CarsService){
     this.obterCarros();
   }
 
+  
   obterCarros(){
     return this.carService.obterCarros()
-      .subscribe(car => this.carros = car)
+      .subscribe(car => console.log(car))
   }
+
+  obterCarro(){
+    return this.carService.obterCarro(2)
+      .subscribe(carro => console.log(carro))
+  }
+
+  addCarro(): void{
+    const carro: iCar = {
+     model: "hhh",
+     year: 2022,
+     color: 'red'
+    };
+
+    this.carService.addCarro(carro)
+      .subscribe(carro => console.log(carro))
+  }
+
+  atualizarCarro(): void{
+    const carro: iCar = {
+      id: 3,
+      model: "jkjk",
+      year: 2000,
+      color: "Blue" 
+     };
+
+    this.carService.atualizarCarro(carro)
+     .subscribe(carro => console.log(carro));
+  }
+
+  deleteCarro(){
+    this.carService.deleteCarro(2)
+      .subscribe(res => console.log(res + "deletado"));
+  }
+
 }
--- src/app/services/cars.service.ts
+++ src/app/services/cars.service.ts
@@ -15,4 +15,20 @@ export class CarsService {
   obterCarros(): Observable<iCar[]>{
     return this.httpClient.get<iCar[]>(`${API_PATH}cars`)
   }
+
+  obterCarro(id: number): Observable<iCar>{
+    return this.httpClient.get<iCar>(`${API_PATH}cars/${id}`);
+  }
+
+  addCarro(carro: iCar): Observable<iCar>{
+    return this.httpClient.post<iCar>(`${API_PATH}cars`, carro);
+  }
+
+  atualizarCarro(carro: iCar): Observable<iCar>{
+    return this.httpClient.put<iCar>(`${API_PATH}cars/${carro.id}`, carro);
+  }
+   
+  deleteCarro(carroId: number): Observable<void>{
+    return this.httpClient.delete<void>(`${API_PATH}cars/${carroId}`)
+  }
 }
